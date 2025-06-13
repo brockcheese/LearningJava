b554d315bfcc593505f74683f0238663e043aa78
Pace, Brock James <bpace33@gatech.edu>
2025-06-13 12:24:03 -0500

    Added charat method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,16 +3,18 @@ public class HelloWorld {
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
         boolean hasPerfectAttendance = true;
+
         String studentFirstName = "Kayla";
         String studentLastName = "Harley";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
+
         System.out.println(hasPerfectAttendance);
         System.out.println(studentFirstName);
         System.out.println(studentLastName);
