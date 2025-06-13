996c05c01139d6289008290554061a9a9050c80b
Pace, Brock James <bpace33@gatech.edu>
2025-06-13 12:32:01 -0500

    Added ability to update GPA

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,23 +1,19 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        int studentAge = 15;
         double studentGPA = 3.45;
-        boolean hasPerfectAttendance = true;
-
         String studentFirstName = "Kayla";
         String studentLastName = "Harley";
-        char studentFirstInitial = studentFirstName.charAt(0);
-        char studentLastInitial = studentLastName.charAt(0);
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
+        System.out.println(studentFirstName + " " +
+                studentLastName + " has a GPA of " + studentGPA);
+        System.out.println("What do you want to update it to?");
 
-        System.out.println(hasPerfectAttendance);
-        System.out.println(studentFirstName);
-        System.out.println(studentLastName);
-
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+        System.out.println(studentFirstName + " " +
+                studentLastName + " now has a GPA of " + studentGPA);
     }
 }
